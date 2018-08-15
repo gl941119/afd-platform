14f69a0001376f33f041068702d6b021bd473af9
yuqian <yuqian@bierinc.com>
2018-08-15 19:41:02 +0800

    carousel

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,3 +1,41 @@
 <template>
-    <div>Index</div>
+    <div>
+        <custom-carousel :swiper-imgs="swiperImgs"></custom-carousel>
+    </div>
 </template>
+<script>
+    import customCarouselCom from '@/components/index-com/custom-carousel';
+    import Request from '../../utils/require.js';
+    export default {
+        data() {
+            return {
+                swiperImgs: [],
+            }
+        },
+        mounted() {
+            this.findAdvertisement()
+        },
+        methods: {
+            // carousel image from server
+            findAdvertisement() {
+                return new Promise((resolve, reject) => {
+                    Request({
+                        url: 'FindAdvertisement',
+                        data: { advertType: '1' },
+                        type: 'get',
+                    }).then(res => {
+                        this.swiperImgs = this.handleCarouselData(res.data);
+                        console.log('this.swiperImgs->', this.swiperImgs);
+                        resolve();
+                    })
+                })
+            },
+            handleCarouselData(data) {
+                return data.filter(item => item.advertPosition === 1).sort((a, b) => a.advertSort - b.advertSort)
+            }
+        },
+        components: {
+            'custom-carousel': customCarouselCom,
+        }
+    }
+</script>
