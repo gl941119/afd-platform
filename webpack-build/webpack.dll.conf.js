9888c65bcdb94e16e24a1f21edf2e30500f18481
yuqian <yuqian@bierinc.com>
2018-08-15 09:45:31 +0800

    build succ

--- webpack-build/webpack.dll.conf.js
+++ webpack-build/webpack.dll.conf.js
@@ -3,35 +3,36 @@ const webpack = require('webpack')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-  entry: {
-    vue: ['vue/dist/vue.esm.js', 'vue-router', 'vuex', 'vee-validate', 'axios'],
-    vendor: ['vue-awesome-swiper', 'vue-i18n', 'crypto-js']
-  },
-  output: {
-    path: path.join(__dirname, '../dll'),
-    filename: '[name].dll.js',
-    library: '[name]'
+    entry: {
+        vue: ['vue/dist/vue.esm.js', 'vue-router', 'vuex', 'axios'],
+        ui: ['mint-ui'],
+        vendor: ['vue-i18n', 'crypto-js']
     },
-  devtool: '#source-map',
-  mode: 'production',
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        parallel: true,
-        sourceMap: true,
-        cache: true
-      })
+    output: {
+        path: path.join(__dirname, '../dll'),
+        filename: '[name].dll.js',
+        library: '[name]'
+    },
+    devtool: '#source-map',
+    mode: 'production',
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                parallel: true,
+                sourceMap: true,
+                cache: true
+            })
+        ]
+    },
+    plugins: [
+        new webpack.DllPlugin({
+            path: path.join(__dirname, '../dll', '[name]-manifest.json'),
+            name: '[name]'
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        })
     ]
-  },
-  plugins: [
-    new webpack.DllPlugin({
-      path: path.join(__dirname, '../dll', '[name]-manifest.json'),
-      name: '[name]'
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    })
-  ]
 }
