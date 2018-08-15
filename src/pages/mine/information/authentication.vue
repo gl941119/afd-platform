e526db28cf4b1cc346e3956d1c462e30d236ba1b
gaigai <gaigaiyang@bierinc.com>
2018-08-15 19:53:06 +0800

    auth infor show-footer

--- src/pages/mine/information/authentication.vue
+++ src/pages/mine/information/authentication.vue
@@ -1,10 +1,47 @@
 <template>
-
+    <div class="auth">
+        <div class="auth_info">
+            <mt-field class="auth_info_input" placeholder="姓名" v-model="name"></mt-field>
+            <mt-field class="auth_info_input" placeholder="证件类型" v-model="idType"></mt-field>
+            <mt-field class="auth_info_input" placeholder="身份证号" v-model="idNum"></mt-field>
+            <mt-field class="auth_info_input" placeholder="国家或地区" v-model="country"></mt-field>
+        </div>
+        <div class="auth_next">
+            <mt-button class="auth_next_button" type="primary" popup_box_nickname_input size="large">下一步</mt-button>
+        </div>
+    </div>
 </template>
 <script>
+    import Cache from '../../../utils/cache.js'
     export default {
-
+        data() {
+            return {
+                name: '',
+                idType: '',
+                idNum: '',
+                country: '',
+            }
+        },
+        created() {
+            Cache.setSession('show_footer', '0');
+            this.$store.commit('setShowFooter', '0');
+        },
     }
 </script>
 <style lang="scss" scoped>
+    @import '../../../assets/css/global.scss';
+    .auth {
+        &_info {
+            @include remCalc(padding, 0, 38px);
+            margin-top: pxTorem(50px);
+        }
+        &_next {
+            @include remCalc(padding, 0, 38px);
+            @include remCalc(margin, 25px, 35px, 0);
+            &_button {
+                background: rgba(0, 158, 194, 1);
+                font-size: 14px;
+            }
+        }
+    }
 </style>
--- src/pages/mine/information/information.vue
+++ src/pages/mine/information/information.vue
@@ -2,11 +2,11 @@
     <div class="information">
         <mt-cell class="information_kind" @click.native="active" title="头像" is-link>
         </mt-cell>
-        <mt-cell class="information_kind" @click.native="nickname" title="昵称" is-link>
+        <mt-cell class="information_kind" @click.native="openInfo(1)" title="昵称" is-link>
         </mt-cell>
-        <mt-cell class="information_kind" @click.native="loginPassword" title="登录密码" is-link>
+        <mt-cell class="information_kind" @click.native="openInfo(2)" title="登录密码" is-link>
         </mt-cell>
-        <mt-cell class="information_kind" title="交易密码" is-link>
+        <mt-cell class="information_kind" @click.native="openInfo(3)" title="交易密码" is-link>
         </mt-cell>
         <mt-cell class="information_kind" title="绑定Telegram" is-link>
         </mt-cell>
@@ -14,18 +14,27 @@
         </mt-cell>
         <mt-actionsheet :actions="actions" v-model="sheetVisible">
         </mt-actionsheet>
-        <div class="popup" v-if="proup">
+        <div class="popup" v-if="proup" @click="openInfo">
             <div class="popup_box">
                 <div class="popup_box_nickname" v-if="nicknameShow">
                     <p class="popup_box_nickname_title">新的昵称</p>
                     <mt-field class="popup_box_nickname_input" v-model="nicknameValue"></mt-field>
                     <mt-button class="popup_box_nickname_button" type="primary" popup_box_nickname_input size="large">确认</mt-button>
                 </div>
-                <div class="popup_box_nickname loginPassword" v-if="loginPasswordShow">
+                <div class="popup_box_nickname passwordBox" v-if="loginPasswordShow">
                     <p class="popup_box_nickname_title">重设登录密码</p>
                     <mt-field class="popup_box_nickname_input" placeholder="原密码" v-model="oldPassword"></mt-field>
                     <mt-field class="popup_box_nickname_input" placeholder="新密码" v-model="password"></mt-field>
                     <mt-field class="popup_box_nickname_input" placeholder="重新输入新密码" v-model="oncePassword"></mt-field>
+                    <mt-field class="popup_box_nickname_input" placeholder="邮箱验证码" v-model="emailCode"></mt-field>
+                    <p class="popup_box_nickname_code">获取邮箱验证码</p>
+                    <mt-button class="popup_box_nickname_button" type="primary" popup_box_nickname_input size="large">确认</mt-button>
+                </div>
+                <div class="popup_box_nickname passwordBox" v-if="tradePasswordShow">
+                    <p class="popup_box_nickname_title">重设交易密码</p>
+                    <mt-field class="popup_box_nickname_input" placeholder="原密码" v-model="oldTradepassword"></mt-field>
+                    <mt-field class="popup_box_nickname_input" placeholder="新密码" v-model="tradepassword"></mt-field>
+                    <mt-field class="popup_box_nickname_input" placeholder="重新输入新密码" v-model="onceTradepassword"></mt-field>
                     <mt-field class="popup_box_nickname_input" placeholder="邮箱验证码" v-model="emailCode"></mt-field>
                     <p class="popup_box_nickname_code">获取邮箱验证码</p>
                     <mt-button class="popup_box_nickname_button" type="primary" popup_box_nickname_input size="large">确认</mt-button>
@@ -35,20 +44,29 @@
     </div>
 </template>
 <script>
+    import Cache from '../../../utils/cache.js'
     export default {
         data() {
             return {
                 actions: [{ name: '从相册中选择', method: this.album }],
                 sheetVisible: false,
                 proup: false,
-                nicknameShow: false,
+                nicknameShow: false, //昵称
                 nicknameValue: '',
-                loginPasswordShow: false,
+                loginPasswordShow: false, //登录密码
                 oldPassword: '',
                 password: '',
                 oncePassword: '',
                 emailCode: '',
+                tradePasswordShow: false, //交易密码
+                oldTradepassword: '',
+                tradepassword: '',
+                onceTradepassword: '',
             }
+        },
+        created() {
+            Cache.setSession('show_footer', '0');
+            this.$store.commit('setShowFooter', '0');
         },
         methods: {
             active() { //选择相册-show
@@ -60,14 +78,28 @@
             proupShow() { //蒙版
                 this.proup = !this.proup;
             },
-            nickname() { //nickname-show
+            openInfo(value) {
                 this.proupShow();
-                this.nicknameShow = !this.nicknameShow;
+                this.kinds(value);
             },
-            loginPassword() {
-                this.proupShow();
-                this.loginPasswordShow = !this.loginPasswordShow;
-            }
+            kinds(value) {
+                switch (value) {
+                case 1:
+                    this.nicknameShow = !this.nicknameShow; //nickname-show
+                    break;
+                case 2:
+                    this.loginPasswordShow = !this.loginPasswordShow; //loginPassword-show
+                    break;
+                case 3:
+                    this.tradePasswordShow = !this.tradePasswordShow; //tradepassword-show
+                    break;
+                default:
+                    this.nicknameShow = false; //nickname-show
+                    this.loginPasswordShow = false; //loginPassword-show
+                    this.tradePasswordShow = false; //tradepassword-show
+                    break;
+                }
+            },
         }
     }
 </script>
@@ -119,7 +151,7 @@
                     }
                 }
             }
-            .loginPassword {
+            .passwordBox {
                 height: pxTorem(328px);
             }
         }
