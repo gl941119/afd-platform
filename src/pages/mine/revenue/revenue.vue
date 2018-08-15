a17c09b7332bdfb25d217ad7383296fc94990bce
gaigai <gaigaiyang@bierinc.com>
2018-08-16 00:47:38 +0800

    invite-purse-data revenue-style-data common-style

--- src/pages/mine/revenue/revenue.vue
+++ src/pages/mine/revenue/revenue.vue
@@ -11,10 +11,22 @@
         </mt-navbar>
         <mt-tab-container v-model="selected">
             <mt-tab-container-item id="1">
-                <mt-cell v-for="n in 20" :key="n" :title="'餐厅 ' + n" />
+                <div class="revenue_title" v-for="(item, index) in flowDatas" :key="index">
+                    <div class="revenue_title_info">
+                        {{item.createTime}}
+                    </div>
+                    <div class="revenue_title_info">{{item.desc}}</div>
+                    <div class="revenue_title_info">{{item.status}}</div>
+                </div>
             </mt-tab-container-item>
             <mt-tab-container-item id="2">
-                <mt-cell v-for="n in 4" :key="n" :title="'餐厅 ' + n" />
+                <div class="revenue_title" v-for="(item, index) in flowDatas" :key="index">
+                    <div class="revenue_title_info">
+                        {{item.createTime}}
+                    </div>
+                    <div class="revenue_title_info">{{item.money}} AFDT</div>
+                    <div class="revenue_title_info">{{item.status}}</div>
+                </div>
             </mt-tab-container-item>
         </mt-tab-container>
         <div class="revenue_buttoneBox">
@@ -23,19 +35,55 @@
     </div>
 </template>
 <script>
-    import Cache from '../../../utils/cache.js'
+    import Cache from '../../../utils/cache.js';
+    import Request from '../../../utils/require';
     export default {
         data() {
             return {
                 selected: '1',
-                earnings: [],
-                withdrawals: [],
+                accountId: this.$store.state.id || Cache.getSession('bier_userid'),
+                flowDatas: [],
             }
         },
         created() {
             Cache.setSession('show_footer', '0');
             this.$store.commit('setShowFooter', '0');
         },
+        mounted() {
+            this.BasicInformation();
+        },
+        watch: {
+            selected() {
+                this.revenueDatas();
+            }
+        },
+        methods: {
+            BasicInformation() {
+                Request({
+                    url: 'QueryRevenueBasicInformation',
+                    data: { accountId: this.accountId },
+                    type: 'get'
+                }).then(res => {
+                    this.id = res.data.id;
+                    this.revenueDatas();
+                })
+            },
+            revenueDatas() {
+                Request({
+                    url: 'QueryRevenueAccountFlow',
+                    data: {
+                        incomeId: this.id,
+                        page: 1,
+                        pageSize: 30,
+                        flowType: this.selected,
+                    },
+                    type: 'get'
+                }).then(res => {
+                    this.flowDatas = res.data;
+                    // this.totals = res.total;
+                })
+            },
+        }
     }
 </script>
 <style lang="scss" scoped>
@@ -52,16 +100,15 @@
             }
         }
         &_tabbar {
-            height: pxTorem(40px);
             display: flex;
             align-items: center;
             justify-content: center;
             border-bottom: 1px solid #D8D8D8;
             background: #ffffff;
-            height: pxTorem(40px);
             text-align: center;
             &_one {
                 border-right: 1px solid #D8D8D8;
+                box-sizing: border-box;
             }
         }
         &_buttoneBox {
@@ -79,6 +126,19 @@
                 height: pxTorem(35px);
                 width: 100%;
                 font-size: 14px;
+            }
+        }
+        &_title {
+            height: pxTorem(40px);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-bottom: 1px solid #D8D8D8;
+            background: #ffffff;
+            text-align: center;
+            &_info {
+                width: 100%;
+                text-align: center;
             }
         }
     }
