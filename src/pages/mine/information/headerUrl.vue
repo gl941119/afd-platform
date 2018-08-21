d5ca9dd6d56e854e3ad9bc32894f2690a65e16e5
gaigai <gaigaiyang@bierinc.com>
2018-08-21 19:40:45 +0800

    headerUrl

--- src/pages/mine/information/headerUrl.vue
+++ src/pages/mine/information/headerUrl.vue
@@ -31,6 +31,7 @@
     export default {
         data() {
             return {
+                accountId: this.$store.state.id || Cache.getSession('bier_userid'),
                 headUrl: this.$store.state.heardUrl || 'https://s3-us-west-2.amazonaws.com/static-afd/upload-folder/picture/0ce0fa3b61824c05a3b797adc921150b.png',
                 // avatar
                 uploadImg: Config.UploadImg,
