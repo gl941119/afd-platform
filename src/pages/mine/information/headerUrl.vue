9b7e82a5db8dee46df042ffa6b5cb99d56fd6db2
gaigai <gaigaiyang@bierinc.com>
2018-08-29 10:42:12 +0800

    headerUrl-red

--- src/pages/mine/information/headerUrl.vue
+++ src/pages/mine/information/headerUrl.vue
@@ -42,7 +42,7 @@
             heardUrl() {
                 return this.$store.state.heardUrl || 'https://s3-us-west-2.amazonaws.com/static-afd/upload-folder/picture/0ce0fa3b61824c05a3b797adc921150b.png';
             },
-    },
+        },
         watch: {
             edit(value) {
                 // console.log('edit->', value);
