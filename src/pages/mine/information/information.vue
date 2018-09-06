bff987d9a4f6538216a845936ff6130b95ab00ec
gaigai <gaigaiyang@bierinc.com>
2018-09-06 20:58:08 +0800

    information van.js

--- src/pages/mine/information/information.vue
+++ src/pages/mine/information/information.vue
@@ -2,7 +2,7 @@
     <div class="information">
         <header-nav linkName="mine" isBlue=true title="个人资料"></header-nav>
         <div class="information-item">
-            <van-cell title="头像" class="information-item-kind headerUrl" is-link>
+            <van-cell title="头像" class="information-item-kind headerUrl" @click="changeHeader()" is-link>
                 <van-icon slot="right-icon">
                     <img class="information-item-kind-img" :src="headUrl" />
                     <div class="information-item-kind-fontBox">
@@ -31,6 +31,13 @@
                 <input v-model="nicknames" class="information-input-change"/>
             </div>
         </van-dialog>
+        <van-actionsheet
+            v-model="photo"
+            :actions="actions"
+            cancel-text="取消"
+            @select="onSelect"
+            @cancel="onCancel"
+        />
     </div>
 </template>
 <script>
@@ -44,8 +51,17 @@
                 accountId: this.$store.state.id,
                 nickname: this.$store.state.usernickname,
                 headUrl: 'https://s3-us-west-2.amazonaws.com/static-afd/upload-folder/picture/0ce0fa3b61824c05a3b797adc921150b.png',
-                show: false,
                 nicknames: '',
+                show: false,
+                photo: false,
+                actions: [
+                    {
+                        name: '拍照',
+                    },
+                    {
+                        name: '从手机相册选择',
+                    },
+                ],
             };
         },
         mounted() {
@@ -64,6 +80,15 @@
                         this.heardUrl = res.data.heardUrl;
                     }
                 });
+            },
+            changeHeader() {
+                this.photo = true;
+            },
+            onSelect(item) {
+                console.log(item);
+            },
+            onCancel() {
+                this.photo = false;
             },
             changeUserName() {
                 this.nicknames = '';
