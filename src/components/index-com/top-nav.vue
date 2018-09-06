6f314b5d77ccbef339b37a33b73886bc8e8a7024
yuqian <yuqian@bierinc.com>
2018-09-06 16:47:36 +0800

    index earn and nav

--- src/components/index-com/top-nav.vue
+++ src/components/index-com/top-nav.vue
@@ -1,86 +1,28 @@
 <template>
     <div class="header">
-        <div class="header-item">
-            <i class="custom-mint-icon-custom-activity"></i>
-            <span>精彩活动</span>
-        </div>
-        <div @click="advertIncome" class="header-item">
-            <i class="custom-mint-icon-guanggao"></i>
-            <span>广告收益</span>
-        </div>
-        <div @click="inviteIncome" class="header-item">
-            <i class="custom-mint-icon-yaoqing"></i>
-            <span>邀请收益</span>
-        </div>
-        <div class="header-item">
-            <i class="custom-mint-icon-gonglve"></i>
-            <span>挖矿攻略</span>
-        </div>
+        <a href="javascript:;">
+            <img src="../../assets/imgs/img/freshman.png">
+        </a>
+        <a href="javascript:;">
+            <img src="../../assets/imgs/img/activity.png">
+        </a>
     </div>
 </template>
 <script>
-import Cache from '../../utils/cache.js';
-    export default {
-        name: 'TopNav',
-        data() {
-            return {}
-        },
-        methods: {
-            advertIncome() {
-                if (!(this.$store.state.token || Cache.getSession("bier_token"))) {
-                    this.$messagebox.confirm(
-                        '请先登录，再查看收益'
-                    ).then(() => {
-                        this.$router.push({ name: 'login' })
-                    }, () => {});
-                    return;
-                }
-                this.$router.push({name:'revenue'})
-            },
-            inviteIncome() {
-                if (!(this.$store.state.token || Cache.getSession("bier_token"))) {
-                    this.$messagebox.confirm(
-                        '请先登录，才能邀请好友'
-                    ).then(() => {
-                        this.$router.push({ name: 'login' })
-                    }, () => {});
-                    return;
-                }
-                this.$router.push({name:'invite'})
-            },
-        }
-    }
+// import Cache from '../../utils/cache.js';
+export default {
+    name: 'TopNav',
+    data() {
+        return {};
+    },
+};
 </script>
 <style lang="scss" scoped>
     @import '../../assets/css/global.scss';
     .header {
-        width: 100vw;
-        height: pxTorem(94px);
-        @include content-flex(space-around);
-        &-item {
-            @include content-flex();
-            flex-direction: column;
-            i {
-                padding: 6px;
-                background: #B656CA;
-                color: #fff;
-                font-size: pxTorem(28px);
-                border-radius: 5px;
-                &[class$="-guanggao"] {
-                    background: #519CF3;
-                }
-                &[class$="-yaoqing"] {
-                    background: #4EDEBE;
-                }
-                &[class$="-gonglve"] {
-                    background: #9243EE;
-                }
-            }
-            span {
-                margin-top: 6px;
-                color: #333;
-                font-size: pxTorem(13px);
-            }
-        }
+        margin: 4px 0;
+        background: #fff;
+        @include remCalc(padding, 10, 29, 16);
+        @include content-flex(space-between);
     }
 </style>
